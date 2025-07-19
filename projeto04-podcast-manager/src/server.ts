59926eb92bf8638e818a4ad840bc767530148d10
Ana Valeska Santos <avaleska2908@gmail.com>
2025-07-19 17:15:45 -0300

    :sparkles: feat: Adds http module and starts the server.

--- projeto04-podcast-manager/src/server.ts
+++ projeto04-podcast-manager/src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import * as http from "http";
+
+const server = http.createServer(
+  (request: http.IncomingMessage, response: http.ServerResponse) => {}
+);
+
+const port = process.env.PORT;
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${port}.`);
+});
