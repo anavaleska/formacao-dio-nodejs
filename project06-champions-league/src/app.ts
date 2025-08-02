91b25f03f493c881223c320f83df3bb5d9481465
Ana Valeska Santos <avaleska2908@gmail.com>
2025-08-02 05:29:42 -0300

    :sparkles: feat: Implements CORS and add more clubs to the database.

--- project06-champions-league/src/app.ts
+++ project06-champions-league/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { router } from "./routes/routes";
+import cors from "cors";
 
 export const createApp = () => {
   const app = express();
 
   app.use(express.json());
   app.use("/api", router);
+  app.use(cors());
 
   return app;
 };
